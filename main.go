bb19a937ab9f210489a60739c34078f2d9f69a2e
Jaesung Ryu <cwryu6252@naver.com>
2022-02-14 23:22:01 +0900

    func

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
+func lenAndUpper(name string) (length int, uppercase string){
+	defer fmt.Println("I'm Done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
 
 func main() {
 	fmt.Println("Hello World")
+	repeatMe("Hello", "World")
+	lenth, uppercase := lenAndUpper("Hello World")
+	fmt.Println(lenth, uppercase)
 }
