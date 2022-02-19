4d08b1b0cdd90a6f573d7a160c86101a4f2bb59d
Jaesung Ryu <cwryu6252@naver.com>
2022-02-19 23:06:43 +0900

    3.1

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+)  
 
-	"github.com/Haebuk/learngo/mydict"
-)
+var errRequestFailed = errors.New("request failed")
+var results = make(map[string]string)
 
 func main() {
-	dictionary := mydict.Dictionary{}
-	baseword := "hello"
-	dictionary.Add(baseword, "First")
-	dictionary.Search(baseword)
-	dictionary.Delete(baseword)
-	word, err := dictionary.Search(baseword)
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Print(word)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+		}
+
+	for _, url := range urls{
+		result := "OK"
+		err := hitURL(url)
+		if err != nil{
+			result = "FAILED"
+		}
+		results[url] = result
 	}
+	for url, result := range results{
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error{
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+	
 }
