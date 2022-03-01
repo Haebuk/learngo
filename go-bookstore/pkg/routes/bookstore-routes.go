f2a31f472e45984cdd5a95fcb8d3c8d53af6a167
Jaesung Ryu <cwryu6252@naver.com>
2022-03-01 16:40:52 +0900

    done

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"github.com/haebuk/learngo/go-bookstore/pkg/controllers"
+)
 
 var RegisterBookStoreRoutes = func (router *mux.Router)  {
-	router.Handle("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controller.GetBook).Methods("GET")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
--- go-bookstore/pkg/utils/utils.go
+++ go-bookstore/pkg/utils/utils.go
@@ -3,10 +3,10 @@ package utils
 import (
 	"encoding/json"
 	"io/ioutil"
-	"net/http"
+	"net/http" 
 )
 
-func ParseBody(r *http.Request, X interface{}){
+func ParseBody(r *http.Request, x interface{}){
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil{
 			return
