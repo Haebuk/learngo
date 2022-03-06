9906e5ca11824d842178b4314d79e6727cd36e8f
Jaesung Ryu <cwryu6252@naver.com>
2022-03-06 18:12:54 +0900

    6-2

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -10,22 +10,33 @@ import (
 
 const port string = ":4000"
 
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
 type URLDescription struct {
-	URL string `json:"url"`
+	URL URL `json:"url"`
 	Method string `json:"method"`
 	Description string `json:"description"`
 	Payload string `json:"data:string,omitempty"`
 }
 
+func (u URLDescription) String() string {
+	return "Hello I'm the URL Description"
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL: "/",
+			URL: URL("/"),
 			Method: "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL: "/block",
+			URL: URL("/block"),
 			Method: "POST",
 			Description: "Add A Block",
 			Payload: "data:string",
