60866328fb81303afcaaf1dbdfe383f351746e70
Jaesung Ryu <cwryu6252@naver.com>
2022-03-06 18:45:15 +0900

    6-4

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -1,78 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/learngo/nomadcoin/blockchain"
-	"github.com/learngo/nomadcoin/utils"
+	"github.com/learngo/nomadcoin/explorer"
+	"github.com/learngo/nomadcoin/rest"
 )
 
-const port string = ":4000"
-
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL URL `json:"url"`
-	Method string `json:"method"`
-	Description string `json:"description"`
-	Payload string `json:"data:string,omitempty"`
-}
-
-type AddBlockBody struct {
-	Message string
-}
-
-func (u URLDescription) String() string {
-	return "Hello I'm the URL Description"
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL: URL("/"),
-			Method: "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL: URL("/block"),
-			Method: "POST",
-			Description: "Add A Block",
-			Payload: "data:string",
-		},
-		{
-			URL: URL("/block/{id}"),
-			Method: "GET",
-			Description: "See A Block",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
-	case "POST":
-		var addBlockBody AddBlockBody
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
 
 func main() {
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	go explorer.Start(3000)
+	rest.Start(4000)
  }
