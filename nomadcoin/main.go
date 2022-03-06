bd263b3affbd6b22c16368910a19a4573dc98ad7
Jaesung Ryu <cwryu6252@naver.com>
2022-03-06 18:29:45 +0900

    6-3

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/learngo/nomadcoin/blockchain"
+	"github.com/learngo/nomadcoin/utils"
 )
 
 const port string = ":4000"
@@ -22,6 +24,10 @@ type URLDescription struct {
 	Method string `json:"method"`
 	Description string `json:"description"`
 	Payload string `json:"data:string,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
 }
 
 func (u URLDescription) String() string {
@@ -41,13 +47,32 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "Add A Block",
 			Payload: "data:string",
 		},
+		{
+			URL: URL("/block/{id}"),
+			Method: "GET",
+			Description: "See A Block",
+		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
  }
