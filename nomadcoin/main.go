940e5c4bf0866084fabc5b2c382d102d69d861e5
Jaesung Ryu <cwryu6252@naver.com>
2022-03-07 21:46:58 +0900

    7-2

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -18,11 +19,17 @@ func main() {
 	if len(os.Args) < 2 {
 		usage()
 	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
 		fmt.Println("Start REST API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
 	}
