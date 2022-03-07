19bf3b6b33b0cb06f9ef223ff9014ce11d4727d2
Jaesung Ryu <cwryu6252@naver.com>
2022-03-07 21:37:11 +0900

    7-1

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
 )
+
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Println("Please use the following commands:")
+	fmt.Println("explorer:	Start the HTML Explorer")
+	fmt.Println("rest:		Start the REST API(recommended)")
+	os.Exit(0)
+}
 
 
 func main() {
- }
+	if len(os.Args) < 2 {
+		usage()
+	}
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+ } 
