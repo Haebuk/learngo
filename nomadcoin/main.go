fe5b237d8a755c6e0b77b21aac7d055979c59271
Jaesung Ryu <cwryu6252@naver.com>
2022-03-09 20:06:23 +0900

    8-6

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/learngo/nomadcoin/blockchain"
+import (
+	"github.com/learngo/nomadcoin/blockchain"
+	"github.com/learngo/nomadcoin/cli"
+)
 
 
 
 func main() {
 	blockchain.Blockchain()
+	cli.Start()
 } 
