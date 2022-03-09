8c071b97219f119b0051f78f491153b58c4e8056
Jaesung Ryu <cwryu6252@naver.com>
2022-03-09 20:27:23 +0900

    8-8

--- nomadcoin/db/db.go
+++ nomadcoin/db/db.go
@@ -31,7 +31,7 @@ func DB() *bolt.DB {
 }
 
 func Close() {
-	
+
 }
 
 func SaveBlock(hash string, data []byte) {
@@ -43,7 +43,7 @@ func SaveBlock(hash string, data []byte) {
 	utils.HandleErr(err)
 }
 
-func SaveBlockchain(data []byte) {
+func SaveCheckpoint(data []byte) {
 	err := DB().Update(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte(dataBucket))
 		err := bucket.Put([]byte(checkpoint), data)
