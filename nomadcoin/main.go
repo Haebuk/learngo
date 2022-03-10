282b1bc67ed0d95cd6859f3eab6dc1fd28a17084
Jaesung Ryu <cwryu6252@naver.com>
2022-03-10 18:42:33 +0900

    9-2

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/learngo/nomadcoin/cli"
+	"github.com/learngo/nomadcoin/db"
 )
 
 
 
 func main() {
-	difficulty := 6
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello" + fmt.Sprint(nonce))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			return 
-		} else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 } 
