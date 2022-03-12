7a3d6de72520f859bcc2f6858ddf3cd36b5ab341
Jaesung Ryu <cwryu6252@naver.com>
2022-03-12 17:11:10 +0900

    11-5

--- nomadcoin/wallet/wallet.go
+++ nomadcoin/wallet/wallet.go
@@ -1,37 +1,26 @@
 package wallet
 
 import (
-	"crypto/x509"
-	"encoding/hex"
-	"fmt"
-	"math/big"
-
-	"github.com/learngo/nomadcoin/utils"
+	"crypto/ecdsa"
+	"os"
 )
 
-const (
-	signature string = "176692e4212123345493a148db1c04bb7447b448198fbdc8f59c0c8151567c50d3b0f8723b2fef88c144657293bde538f48d17ecf7a62b15be715ba28b1e0dea%"
-	hashedMessage string = "78109dc311bd35b7c9c0bae3938bf6779d3398ddc8968b7dc38a6687"
-	privateKey string = "30770201010420d7388bd93ebc0fb8cadef6166ce4aa31d9f55e664a6449b3c5004ec26e7a9abba00a06082a8648ce3d030107a14403420004d31f878f5a05b7dadf61e6bb003849fdeba44bd58d17890a0e2a479505d83a23b77a97ce2640efe32d2408602cb58cc1df139ff6076315e9cd6c574cc7aa8390"
-)
+type wallet struct {
+	privateKey *ecdsa.PrivateKey
+}
 
-func Start() {
-	privBytes, err := hex.DecodeString(privateKey)
-	utils.HandleErr(err)
+var w *wallet
 
-	private, err := x509.ParseECPrivateKey(privBytes)
+func hasWalletFile() bool {
+	_, err := os.Stat("nomadcoin.wallet")
+	return !os.IsNotExist(err)
+}
 
-	utils.HandleErr(err)
-
-	sigBytes, err := hex.DecodeString(signature)
-	rBytes := sigBytes[:len(sigBytes)/2]
-	sBytes := sigBytes[len(sigBytes)/2:]
-	fmt.Printf("%d\n\n%d\n\n%d\n\n", sigBytes, rBytes, sBytes)
-
-	var bigR, bigS = big.Int{}, big.Int{}
-
-	bigR.SetBytes(rBytes)
-	bigS.SetBytes(sBytes)
-
-	fmt.Println(bigR, bigS)
+func Wallet() *wallet {
+	if w == nil {
+		if hasWalletFile() {
+			
+		}
+	}
+	return w
 }
