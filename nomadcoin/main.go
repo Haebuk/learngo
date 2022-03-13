44a607771acd21bf7739a499112c430116bbaf49
Jaesung Ryu <cwryu6252@naver.com>
2022-03-13 20:58:46 +0900

    12-2

--- nomadcoin/main.go
+++ nomadcoin/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"github.com/learngo/nomadcoin/cli"
-	"github.com/learngo/nomadcoin/db"
+	"fmt"
+	"time"
 )
 
-
- 
+func countToTen(c chan int) {
+	for i := range [10]int{} {
+		c <- i
+		time.Sleep(1 * time.Second)
+	}
+}
 
 func main() {
-	// wallet.Wallet()
-	cli.Start()
-	defer db.Close()
+	c := make(chan int)
+	go countToTen(c)
+	for {
+		a := <-c
+		fmt.Println(a)
+	}
 } 
