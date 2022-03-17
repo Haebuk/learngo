b7253bd024339f5e98efef6309fb2277c1b1b5bf
Jaesung Ryu <cwryu6252@naver.com>
2022-03-17 21:11:42 +0900

    12-15

--- nomadcoin/p2p/p2p.go
+++ nomadcoin/p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/learngo/nomadcoin/utils"
@@ -21,16 +20,11 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r ,nil)
 	utils.HandleErr(err)	
 	initPeer(conn, ip, openPort)
-	time.Sleep(20 * time.Second)
-	peer := initPeer(conn, ip, openPort)
-	peer.inbox <- []byte("Hello from 3000!")
 }
 
 func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, address, port)
-	time.Sleep(10 * time.Second)
-	peer.inbox <- []byte("Hello from 4000!")
+	initPeer(conn, address, port)
 }
