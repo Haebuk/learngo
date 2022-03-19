8dafd56d74f3819cb66365b9f760df508dc63059
Jaesung Ryu <cwryu6252@naver.com>
2022-03-19 21:27:13 +0900

    12-20

--- nomadcoin/p2p/messages.go
+++ nomadcoin/p2p/messages.go
@@ -1,4 +1,11 @@
- package p2p
+package p2p
+
+import (
+	"encoding/json"
+
+	"github.com/learngo/nomadcoin/blockchain"
+	"github.com/learngo/nomadcoin/utils"
+)
 
  type MessageKind int
 
@@ -11,4 +18,27 @@
  type Message struct {
 	 Kind MessageKind
 	 Payload []byte
- }
+}
+
+func (m *Message) addPayload(p interface{}) {
+	b, err := json.Marshal(p)
+	utils.HandleErr(err)
+	m.Payload = b
+}
+
+func makeMessage(kind MessageKind, payload interface{}) []byte {
+	m := Message {
+		Kind: kind,
+	}
+	m.addPayload(payload)
+	mJson, err := json.Marshal(m)
+	utils.HandleErr(err)
+	return mJson
+}
+
+func sendNewestBlock(p *peer) {
+	b, err := blockchain.FindBlock(blockchain.Blockchain().NewestHash)
+	utils.HandleErr(err)
+	m := makeMessage(MessageNewestBlock, b)
+	p.inbox <- m
+}
